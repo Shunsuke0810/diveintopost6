62db22f1bdaf82ef0cd26245b6e0e87e94cd9d93
SHUNSUKE <shunsuke0810@gmail.com>
2022-07-27 17:47:19 +0900

    権限移動アクション時,権限が移動されたユーザーにメールが送られるよう追記しました

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -6,6 +6,7 @@ class TeamsController < ApplicationController
   def assign_owner
     @team.update(owner_id: params[:owner_id])
     @user = User.find(@team.owner_id)
+    OwnerChangeMailer.owner_change_mail(@user).deliver
     redirect_to team_path, notice: 'オーナー権限が移動しました!'
   end
 
